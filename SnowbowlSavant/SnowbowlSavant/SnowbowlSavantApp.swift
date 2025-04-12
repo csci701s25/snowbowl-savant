d944eed57b48d594fb50a82d2996a3173d94ee8f
AidMMcMillan <amcmillan@middlebury.edu>
2025-04-11 22:12:37 -0400

    added jupyter notebook for edge detection

---
+++ SnowbowlSavant/SnowbowlSavant/SnowbowlSavantApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowbowlSavantApp.swift
+//  SnowbowlSavant
+//
+//  Created by Aidan and Dermot McMillan on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SnowbowlSavantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnowbowlSavant/SnowbowlSavantTests/SnowbowlSavantTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnowbowlSavantTests.swift
+//  SnowbowlSavantTests
+//
+//  Created by Aidan and Dermot McMillan on 4/4/25.
+//
+
+import Testing
+@testable import SnowbowlSavant
+
+struct SnowbowlSavantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
